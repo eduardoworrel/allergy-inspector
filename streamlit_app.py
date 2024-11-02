c068d02825bd545ef1950f7d98a9a217c6b7fcb9
Laibasif <166300017+Laibasif@users.noreply.github.com>
2024-11-02 22:05:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,12 @@ from ui.sidebar import sidebar_setup
 from ui.media_input import media_input
 from streamlit_chat import message
 
-st.set_page_config(page_title="Your Allergy Detector🕵‍♂", page_icon="🔍")
+st.set_page_config(page_title="Allergy Assistant", page_icon="🤧")
 
 sidebar_setup()
 
 if st.session_state["allergies_selected"]:
-    message("Hello there. What are you about to eat? Is it something you chose... or was it chosen for you? Perhaps there's a pattern, a clue hidden in the ingredients, the flavors, the texture that you don't know. So, hold on buddy. Let us investigate for you.")
+    message("Hello, what are you about to eat?")
     media_input()
 else:
-    st.markdown("# Well, well... what’s on the menu? Just the usual, or something... intriguing?🍽")
+    st.markdown("# Please select your allergies in the sidebar.")
