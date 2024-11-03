c3553da2dbebfb5a94f2f593a2e3f0a829b72210
Sana <165957167+Sanarazaaa@users.noreply.github.com>
2024-11-03 18:22:12 +0500

    Update sidebar.py

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -5,7 +5,6 @@ from utils.session_state import init_session_state
 from utils.media_handler import image_to_base64
 
 # Lista predefinida de categorias de alergias
-# Lista predefinida de categorias de alergias
 allergy_options = [
     "Egg",
     "Milk",
@@ -45,7 +44,6 @@ def sidebar_setup():
     option = st.sidebar.radio("Do you want to know why we are so accurate?", 
                                ("Select an option ❗", "Yes, tell me!", "No, I don't want to know this!"))
 
-
     # Display the additional information only if the user clicks "Yes, tell me!"
     if option == "Yes, tell me!":
         st.sidebar.markdown("Well, we use the powerful Aria model 💪.")
@@ -72,7 +70,6 @@ def sidebar_setup():
                 avatar = image_to_base64(avatar.getvalue())
                 base64avatar = f"data:image/png;base64,{avatar}"
                 st.session_state["user_avatar"] = base64avatar
-            )
             
             # Seletor de alergias
             user_allergies = st.multiselect(
@@ -89,6 +86,7 @@ def sidebar_setup():
                     st.rerun()
                 else:
                     st.warning("Please select at least one allergy.")
+
     if not st.session_state["allergies_selected"]:
         setup()
     else:
@@ -96,9 +94,8 @@ def sidebar_setup():
         if st.sidebar.button("Edit preferences"):
             setup() 
         if st.session_state["user_avatar"] != "":
-            st.sidebar.image(st.session_state["user_avatar"],width=120)
+            st.sidebar.image(st.session_state["user_avatar"], width=120)
         else:
-            st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/434px-Unknown_person.jpg",width=120)
-        st.sidebar.markdown("## "+st.session_state["user_name"] or "Guest")
-        st.sidebar.markdown("⚠️ :gray["+", ".join(st.session_state.get("user_allergies")) + "]")
-
+            st.sidebar.image("https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/434px-Unknown_person.jpg", width=120)
+        st.sidebar.markdown("## " + (st.session_state["user_name"] or "Guest"))
+        st.sidebar.markdown("⚠️ :gray[" + ", ".join(st.session_state.get("user_allergies", [])) + "]")
