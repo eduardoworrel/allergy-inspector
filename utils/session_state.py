2cc93f5308da4d215482f5803e5636a090cbf3a3
eduardoworrel <eduardo.worrel@gmail.com>
2024-11-03 19:20:47 -0300

    reduce allergies

--- utils/session_state.py
+++ utils/session_state.py
@@ -13,8 +13,8 @@ def init_session_state():
         st.session_state["processing"] = False
         st.session_state["load_video"] = 0
         st.session_state["allergy_options"] = [ 
-            "Nuts", "Peanuts", "Tree Nuts", "Dairy", "Lactose", "Gluten", "Wheat", "Seafood",
-            "Shellfish", "Fish", "Soy", "Eggs", "Sesame", "Corn", "Mustard", "Celery", 
-            "Sulfites", "Legumes", "Nightshades", "Fruits", "Vegetables", "Chocolate",
+            "Nuts", "Dairy", "Gluten", "Seafood",
+            "Soy", "Eggs", "Sesame", "Corn", "Mustard", "Celery", 
+            "Sulfites", "Legumes", "Nightshades", "Chocolate",
             "Alcohol", "Caffeine", "Pork", "Red Meat", "Garlic", "Onion", "Spices", "Lupin","Poppy seeds"
-        ] 
+        ]
