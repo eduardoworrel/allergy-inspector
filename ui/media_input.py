7f9d12ad0cdfdd70d8e1f545b85cc054c72c8c5f
Laibasif <166300017+Laibasif@users.noreply.github.com>
2024-11-04 16:54:36 +0500

    Update media_input.py

--- ui/media_input.py
+++ ui/media_input.py
@@ -161,8 +161,7 @@ def check_allergies(ingredients_text):
               
             message("Learn more about your allergies, we are preparing a video. this may take a while.", logo=bot_image)
             with st.spinner('Wait for it...'):
-                message("implementing video")
-                # allergies = ", ".join(allergies) 
+                               # allergies = ", ".join(allergies) 
                 # values = generate_videos(allergies)
                 # for value in values:
                 #     message(value, logo=bot_image)
